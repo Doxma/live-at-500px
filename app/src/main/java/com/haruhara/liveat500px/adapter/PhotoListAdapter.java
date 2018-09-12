da371e97fe36f2a0f582e046b75457d62c1a2b89
lee.k MAC <destinyhero1238@gmail.com>
2018-09-12 17:03:42 +0700

    listview 1 type

--- app/src/main/java/com/haruhara/liveat500px/adapter/PhotoListAdapter.java
+++ app/src/main/java/com/haruhara/liveat500px/adapter/PhotoListAdapter.java
@@ -25,36 +25,14 @@ public class PhotoListAdapter extends BaseAdapter {
         return 0;
     }
 
-    // how many type of view
-    @Override
-    public int getViewTypeCount() {
-        return 2;
-    }
-
-    // get type (before get content)
-    @Override
-    public int getItemViewType(int position) {
-        return position % 2 == 0 ? 0 : 1;
-        // photo is 0, 2, 4, ... so %2 return 0, otherwise textview return 1
-    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if (getItemViewType(position) == 0) {
-            PhotoListItem item;
-            if (convertView != null)
-                item = (PhotoListItem) convertView;
-            else
-                item = new PhotoListItem(parent.getContext());
-            return item;
-        } else {
-            TextView item;
-            if (convertView != null)
-                item = (TextView) convertView;
-            else
-                item = new TextView(parent.getContext());
-            item.setText("Position : " + position);
-            return item;
-        }
+        PhotoListItem item;
+        if (convertView != null)
+            item = (PhotoListItem) convertView;
+        else
+            item = new PhotoListItem(parent.getContext());
+        return item;
     }
 }
